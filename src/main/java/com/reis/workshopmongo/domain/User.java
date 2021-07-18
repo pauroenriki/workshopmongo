84f07eac12a504cf9c7a2468dc95c70c2d4a0a3f
pauroenriki <pauroenriki@gmail.com>
2021-07-18 14:08:44 -0300

    recuperando dados do mongo

--- src/main/java/com/reis/workshopmongo/domain/User.java
+++ src/main/java/com/reis/workshopmongo/domain/User.java
@@ -2,12 +2,16 @@ package com.reis.workshopmongo.domain;
 
 import java.io.Serializable;
 
-import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+@Document
 @Data
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String nome;
 	private String email;
---
+++ src/main/java/com/reis/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.reis.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reis.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
