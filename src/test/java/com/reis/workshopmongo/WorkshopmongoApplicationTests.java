687cf84c18569796ea10b6c9cffbf2a7fb598fa0
pauroenriki <pauroenriki@gmail.com>
2021-07-18 13:32:30 -0300

    primeiro commit

---
+++ src/test/java/com/reis/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reis.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
