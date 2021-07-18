162e7b71ce9f4d9fcd09ea29c85b12adb57dc2b7
pauroenriki <pauroenriki@gmail.com>
2021-07-18 14:20:57 -0300

    instanciação da base de dados

---
+++ src/main/java/com/reis/workshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.reis.workshopmongo.config;
+
+import com.reis.workshopmongo.repository.UserRepository;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.reis.workshopmongo.domain.User;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	@Autowired
+	private UserRepository usuRep;
+
+	@Override
+	public void run(String... args) throws Exception {
+		usuRep.deleteAll();
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		usuRep.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
