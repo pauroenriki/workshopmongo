1a541009633de50f6d5025cf70cc35da6aaa2fc2
pauroenriki <pauroenriki@gmail.com>
2021-07-20 13:59:31 -0300

    Consulta simples com query methods

--- src/main/java/com/reis/workshopmongo/resources/UserResource.java
+++ src/main/java/com/reis/workshopmongo/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.reis.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reis.workshopmongo.domain.Post;
+import com.reis.workshopmongo.resources.util.URL;
 import com.reis.workshopmongo.services.PostService;
 
 @RestController
@@ -18,8 +22,14 @@ public class UserResource {
 	private PostService service;
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Post> findAll(@PathVariable("id") String id) {
+	public ResponseEntity<Post> findById(@PathVariable("id") String id) {
 		return ResponseEntity.ok().body(service.findById(id));
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		return ResponseEntity.ok().body(service.findByTitle(text));
 	}
 
 }
